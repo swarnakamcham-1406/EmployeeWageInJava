1f115109c82f56069cac436bc2bc1cdde483f7e2
kamchamswarna <kamchamswarna@gmail.com>
2020-10-18 12:17:06 +0530

    employee wage per month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -7,30 +7,38 @@ public class EmployeeWageComputation {
 
 		int isPartTime=1;
 		int isFullTime=2;
-		int  empRatePerHr=20;
+		int empRatePerHr=20;
 		int empHr;
+		int totalSalary=0;
 		int salary=0;
+		int numWorkingDays=20;
+			for(int day=1;day<=numWorkingDays; day++)
+      			{
+
 		double empcheck=Math.floor(Math.random() * 10) % 3;
 		int intValue=(int) Math.round(empcheck);
 			switch (intValue) {
 			  case 1:
 					empHr=8;  
-				salary=empHr * empRatePerHr;
-				System.out.println("The Salary is :" + salary);
+				
                                 break;			
           			case 2:
 
             				empHr=12;
-	         			salary=empHr * empRatePerHr;
-		         		System.out.println("The Salary is :" + salary);
-					break;
+	         			
+		
+				break;
 				default:
-				empHr=0;
-				System.out.println("The Salary is :" + salary);
-					break;
+				         empHr=0;
+				break;
 			}
 			  
 
-
+				salary=empHr*empRatePerHr;
+			System.out.println("The salary is :" + salary);
+				
+                        totalSalary=totalSalary+salary;
+			System.out.println("The Total salary is :" + totalSalary);
+}
 }
 }
