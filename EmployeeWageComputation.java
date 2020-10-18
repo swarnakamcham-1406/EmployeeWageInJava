19168a0b52e307f518b120657dc97fc773a363d1
kamchamswarna <kamchamswarna@gmail.com>
2020-10-18 09:55:11 +0530

    Employee Daily Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -5,13 +5,20 @@ public class EmployeeWageComputation {
 
 	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 
-		int IS_FULL_TIME = 1;
-		double empcheck = Math.floor(Math.random() * 10) % 2;
-			if (empcheck == IS_FULL_TIME)
-				System.out.println("Employee is present");
-			else
-				System.out.println("Employee is abscent");
-
+		int isPresent=1;
+		int  empRatePerHr=20;
+		int empHr=8;
+		int salary=0;
+		double empcheck=Math.floor(Math.random() * 10) % 2;
+			if (isPresent == empcheck) {
+			   
+				salary=empHr * empRatePerHr;
+				System.out.println("The Salary is :" + salary);
+			}
+			else {
+				System.out.println("The Salary is :" + salary);
+			}
+			  
 
 
 }
