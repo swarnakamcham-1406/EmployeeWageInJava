698ff65271d3b144ddeeca657d230e898f2eccb5
kamchamswarna <kamchamswarna@gmail.com>
2020-10-18 10:08:05 +0530

    calculate employee Part time Wage using IF condition

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -5,17 +5,24 @@ public class EmployeeWageComputation {
 
 	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 
-		int isPresent=1;
+		int isPartTime=1;
+		int isFullTime=2;
 		int  empRatePerHr=20;
-		int empHr=8;
+		int empHr;
 		int salary=0;
-		double empcheck=Math.floor(Math.random() * 10) % 2;
-			if (isPresent == empcheck) {
-			   
+		double empcheck=Math.floor(Math.random() * 10) % 3;
+			if (isPartTime == 1) {
+			  empHr=8;  
+				salary=empHr * empRatePerHr;
+				System.out.println("The Salary is :" + salary);
+			}
+			else if (isFullTime == 2) {
+				empHr=4;
 				salary=empHr * empRatePerHr;
 				System.out.println("The Salary is :" + salary);
 			}
 			else {
+				empHr=0;
 				System.out.println("The Salary is :" + salary);
 			}
 			  
